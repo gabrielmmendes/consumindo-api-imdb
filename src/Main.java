e9f1f1290d5915d9693333b299987fe9a995e632
Gabriel Mendes <gabrielmendes.0904@gmail.com>
2023-03-27 18:31:07 -0300

    exemplo de requisição http em api

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_o36shslf";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String json = response.body();
+        System.out.println(json);
     }
 }
